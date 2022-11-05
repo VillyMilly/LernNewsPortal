89cadd1dce706683fece1fedab9cdc8540fc4cd2
VillyMilly <96115730+VillyMilly@users.noreply.github.com>
2022-11-05 18:31:50 +0300

    custom filter: censor

--- NewsPortal/baseapp/templatetags/custom_filtres.py
+++ NewsPortal/baseapp/templatetags/custom_filtres.py
@@ -1,9 +1,17 @@
 from django import template
+import string
 
 register = template.Library()
 
-CENSORED_WORDS = []
+CENSORED_WORDS = ['свобода', 'выбор', 'демократия']
+
 
 @register.filter()
-def censor(text: str) -> str:
-    pass
+def censor(texts: str) -> str:
+    list_text = ''.join(i for i in texts if i not in string.punctuation).lower().split(' ')
+    list_of_presence_censored_words = [i for i in list_text if i in CENSORED_WORDS]
+    for i in list_of_presence_censored_words:
+        texts = texts.replace(i.capitalize(), i[0].upper() + '*' * (len(i) - 1))
+        texts = texts.replace(i, i[0] + '*' * (len(i) - 1))
+
+    return texts
